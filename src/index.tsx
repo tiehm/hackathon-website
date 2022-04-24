f5f05539b7e06b0b42f1e0168d84de235e0f3c71
charlie <admin@tiehm.me>
2022-04-25 01:26:04 +0200

    feat: add routing for different pages with react router

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Navigation } from './Navigation';
+import { Event } from './routes/Event';
+import { Join } from './routes/Join';
+import { Team } from './routes/Team';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Navigation />
+          <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/event" element={<Event />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/join" element={<Join />} />
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
